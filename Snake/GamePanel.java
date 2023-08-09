f8aa26e1b459ba9ffab8cf609405adc6ef80ef3f
k31i <115046776+k31i@users.noreply.github.com>
2023-08-09 11:31:41 +0000

    set up from timestamp 7:50

---
+++ Snake/GamePanel.java
@@ -0,0 +1,47 @@
+import javax.swing.*;//these are importing everything form that libary instead of the specific ones
+import java.util.Random;
+import java.awt.*;
+import java.awt.event.*;
+
+
+public class GamePanel extends JPanel implements ActionListener{
+    GamePanel(){
+        
+    }
+    public void startGame(){
+
+    }
+    public void paintComponent(Graphics g){
+
+    }
+    public void draw(Graphics g){
+
+    }
+    public void move(){
+
+    }
+    public void checkApple(){
+
+    }
+    public void checkCollisions(){
+
+    }
+    public void gameOver(Graphics g){
+
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
+    }
+
+    public class MyKeyAdapter extends KeyAdapter{
+        @Override
+        public void keyPressed(KeyEvent e){
+
+        }
+    }
+
+}
